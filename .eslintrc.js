c184c4edd7935eba0f19cc5004302ae1595bd500
Oli Griffiths <oli@tumblr.com>
2018-03-29 14:43:25 -0400

    Add configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,14 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 6,
     sourceType: "module"
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "no-console": 0,
   }
 };
